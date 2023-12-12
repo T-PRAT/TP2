af5765425c010790e804ad3159a614154d3ed829
tprat <Titouan.p@hotmail.fr>
2023-12-12 15:28:54 +0100

    bonus todo

---
+++ main.js
@@ -0,0 +1,66 @@
+// VARIABLES
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const input = document.getElementById('lineWidth');
+
+let isDrawing = false;
+let x = 0;
+let y = 0;
+let currentColor = 'black';
+let lineWidth = 1;
+let isRect = false;
+
+// FUNCTIONS
+function clearCanvas() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function toggleMode() {
+	isRect = !isRect;
+}
+
+const drawLine = (ctx, x1, y1, x2, y2) => {
+	ctx.beginPath();
+	ctx.strokeStyle = currentColor;
+	ctx.lineWidth = lineWidth;
+	ctx.moveTo(x1, y1);
+	ctx.lineTo(x2, y2);
+	ctx.stroke();
+	ctx.closePath();
+}
+
+// EVENTS
+canvas.addEventListener('mousedown', e => {
+	e.preventDefault();
+	x = e.offsetX;
+	y = e.offsetY;
+	isDrawing = true;
+});
+
+canvas.addEventListener('mousemove', e => {
+	if (isDrawing === true) {
+		drawLine(ctx, x, y, e.offsetX, e.offsetY);
+		x = e.offsetX;
+		y = e.offsetY;
+	}
+});
+
+canvas.addEventListener('mouseup', e => {
+	if (isDrawing === true) {
+		drawLine(ctx, x, y, e.offsetX, e.offsetY);
+		x = 0;
+		y = 0;
+		isDrawing = false;
+	}
+});
+
+input.addEventListener('change', e => {
+	lineWidth = e.target.value;
+	input.value = lineWidth;
+	console.log(lineWidth);
+});
+
+document.querySelectorAll('.color-picker').forEach(color => color.addEventListener('click', e => {
+	currentColor = e.target.style.backgroundColor;
+}));
+
